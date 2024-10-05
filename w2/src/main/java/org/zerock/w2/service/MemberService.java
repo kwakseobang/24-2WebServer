84c47e15385b87d76099f98bffe03ee731483746
kwakseobang <iii6602@naver.com>
2024-10-05 18:36:21 +0900

    내부에서 회원 데이터 처리

---
+++ w2/src/main/java/org/zerock/w2/service/MemberService.java
@@ -0,0 +1,34 @@
+package org.zerock.w2.service;
+
+
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.zerock.w2.dao.MemberDAO;
+import org.zerock.w2.domain.MemberVO;
+import org.zerock.w2.dto.MemberDTO;
+import org.zerock.w2.util.MapperUtil;
+
+@Log4j2
+public enum MemberService {
+    //singleton
+    INSTANCE;
+
+    private MemberDAO dao;
+    private ModelMapper modelMapper;
+
+    MemberService() {
+        dao = new MemberDAO();
+
+        modelMapper = MapperUtil.INSTANCE.get();
+    }
+
+    //login
+    public MemberDTO login(String mid, String mpw) throws Exception {
+
+        MemberVO vo = dao.getWithPassword(mid,mpw);
+
+        MemberDTO memberDTO = modelMapper.map(vo,MemberDTO.class);
+
+        return  memberDTO;
+    }
+}
